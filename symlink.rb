3445fd0753d17426d3b81d989e8c0368d1723c65
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-03-23 17:06:49 +0000

    WIP

--- symlink.rb
+++ symlink.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
-dotfiles = %w[.vimrc .zshrc]
+dotfiles = %w[.vim .vimrc .zshrc]
 
 dotfiles.each do |dotfile|
   src = File.join(__dir__, dotfile)
   dest = File.join(Dir.home, dotfile)
 
   if File.exist?(dest)
-    puts "#{dest} already exists, skipping."
+    puts "Skipped: #{dest} already exists"
     next
   end
 
-  system(`ln -s #{src} ~/#{dotfile}`)
+  system(`ln -s #{src} #{dest}`)
+  puts "Symlinked: #{dest}"
 end
