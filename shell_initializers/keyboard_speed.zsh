16e41276de554f2f642d2797280b9168f7e877d5
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-03-23 17:17:11 +0000

    Increase keyboard speed

---
+++ shell_initializers/keyboard_speed.zsh
@@ -0,0 +1,5 @@
+REPEAT_INTERVAL=25
+DELAY=200
+
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval $REPEAT_INTERVAL
+gsettings set org.gnome.desktop.peripherals.keyboard delay $DELAY
