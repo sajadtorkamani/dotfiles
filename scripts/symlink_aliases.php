b25b41020b6d07109272e6b30ee3d95f3459d967
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-03-29 11:24:48 +0100

    Convert Ruby script to PHP

---
+++ scripts/symlink_aliases.php
@@ -0,0 +1,18 @@
+<?php
+#!/usr/bin/env php
+
+$aliasesDir = dirname(__FILE__, 2) . '/aliases';
+$aliases = array_diff(scandir($aliasesDir), ['.', '..']);
+
+foreach($aliases as $alias) {
+    $src = $aliasesDir . '/' . $alias;
+    $dest = $_SERVER['HOME'] . '/.oh-my-zsh/custom/' . $alias;
+
+    if (file_exists($dest)) {
+        echo "Skipping: $dest already exists" . PHP_EOL;
+    }
+    else {
+        symlink($src, $dest);
+        echo "Aliases added: $alias";
+    }
+}
