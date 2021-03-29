0cd6406d6b22fac04f83ce376bf358ed8396f87d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-03-29 11:16:48 +0100

    Add symlink_dotfiles script

---
+++ scripts/symlink_dotfiles.php
@@ -0,0 +1,17 @@
+<?php
+#!/usr/bin/env php
+
+$dotfiles = ['.gitignore_global', '.vim', '.vimrc', '.zshrc'];
+
+foreach ($dotfiles as $dotfile) {
+    $src = dirname(__FILE__, 2) . '/' . $dotfile;
+    $dest = $_SERVER['HOME'] . '/' . $dotfile;
+
+    if (file_exists($dest)) {
+        echo "Skipped: $dest already exists" . PHP_EOL;
+    }
+    else {
+        symlink($src, $dest);
+        echo "Symlinked: $dest points to $src" . PHP_EOL;
+    }
+}
