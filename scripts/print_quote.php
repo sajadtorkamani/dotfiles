27a1554ea6a1b8b12b199dc04c9b9c200e553942
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-04-02 09:28:39 +0100

    Add new quote

--- scripts/print_quote.php
+++ scripts/print_quote.php
@@ -2,6 +2,7 @@
 #!/usr/bin/env php
 
 $quotes = [
+    "Perfectionism is the enemy of progress.",
     'We are all going to die.',
     "Love yourself first because that's who you'll be spending the rest of your life with."
 ];
