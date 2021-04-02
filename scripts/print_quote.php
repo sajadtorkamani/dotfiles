a184352723c5062e0e9854f2cb26219ff73eb305
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-04-02 22:21:58 +0100

    WIP

--- scripts/print_quote.php
+++ scripts/print_quote.php
@@ -2,9 +2,11 @@
 #!/usr/bin/env php
 
 $quotes = [
+    "Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant",
     "Perfectionism is the enemy of progress.",
     'We are all going to die.',
     "Love yourself first because that's who you'll be spending the rest of your life with."
 ];
 
 echo $quotes[array_rand($quotes)];
+
--- shell_initializers/keyboard_speed.zsh
+++ shell_initializers/keyboard_speed.zsh
@@ -1,5 +1,5 @@
-REPEAT_INTERVAL=25
-DELAY=200
+REPEAT_INTERVAL=30
+DELAY=250
 
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval $REPEAT_INTERVAL
 gsettings set org.gnome.desktop.peripherals.keyboard delay $DELAY
