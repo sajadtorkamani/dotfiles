5d2f5e26174e26805ab24c6707a264e4d8a62867
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-04-06 14:28:41 +0100

    Add git alias

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -14,3 +14,5 @@ alias gcb="git checkout"
 alias gp="git push origin HEAD"
 alias gcp="git cherry-pick"
 alias gwip="git add . && git commit -m 'WIP' && git push origin HEAD"
+
+alias gview="gh repo view -w"
