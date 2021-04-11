b4b8bdc17251c6dab822d9b588418eaff6efed51
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-04-11 11:25:00 +0100

    WIP

--- aliases/directories.zsh
+++ aliases/directories.zsh
@@ -1,9 +1,11 @@
 alias docs="cd $HOME/Documents && l"
 alias hosts="$EDITOR /etc/hosts"
+alias confs="cd /etc/nginx/sites-available && l"
 alias code="cd $HOME/code && l"
 alias sites="cd $HOME/sites && l"
 alias fleets="cd $HOME/Downloads/Fleeting && l"
 alias aliases="cd $HOME/code/dotfiles/aliases && l"
+alias downloads="cd $HOME/Downloads && l"
 alias dumps="cd $HOME/dumps && l"
 
 if is_linux; then
--- aliases/misc.zsh
+++ aliases/misc.zsh
@@ -1,2 +1,3 @@
 alias path='echo $PATH | tr -s ":" "\n"'
 alias v="vim"
+alias copy-ssh-key="xclip -selection clipboard < ~/.ssh/id_ed25519.pub"
