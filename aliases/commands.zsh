ef6914905bdbe5f7da5ee5f3db0681e506400b2f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2021-05-21 16:07:52 +0100

    WIP

--- aliases/commands.zsh
+++ aliases/commands.zsh
@@ -9,6 +9,10 @@ alias copy="xclip -selection clipboard < $1"
 
 alias d="docker"
 
-function ssh-docker {
+function ssh-docker() {
   docker container exec -it "$1" bash
 }
+
+if is_linux; then
+  alias open="xdg-open"
+fi
