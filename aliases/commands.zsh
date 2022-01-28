6aed08ec2d51578527e8f872e103b488c57bb341
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-01-28 05:15:53 +0000

    WIP

--- aliases/commands.zsh
+++ aliases/commands.zsh
@@ -20,15 +20,17 @@ alias la="ls -la"
 alias ll="ls -la"
 alias c="clear"
 alias dirsizes="sudo du -sh */ | sort -hr"
-
-alias copy="xclip -selection clipboard < $1"
+alias sizes="du -ahd1"
 
 if is_linux; then
+  alias copy="xclip -selection clipboard < $1"
+  alias clipboard="xclip -sel clip"
   alias open="xdg-open"
 fi
 
 if is_mac; then
   alias chrome="open -a 'Google Chrome'"
+  alias clipboard="pbcopy"
 fi
 
 # Yarn
