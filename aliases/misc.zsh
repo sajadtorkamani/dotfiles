b1fcac6a0a5b5b61e6773560d5b3b1e958405bee
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-19 18:41:31 +0000

    WIP

--- aliases/misc.zsh
+++ aliases/misc.zsh
@@ -1,9 +1,14 @@
 alias path='echo $PATH | tr -s ":" "\n"'
 alias v="vim"
 alias isodate="date +'%Y%m%d%H%M%S'"
+
 function rsp() {
   dc exec php ./bin/console app:reset-roles-and-permissions --summary &&
   dc exec php ./bin/console c:c
+}
+
+function phpcs() {
+  docker-compose exec --user=www-data php vendor/bin/php-cs-fixer --config=.php-cs-fixer.dist.php fix
 }
 
 function download-playlist() {
