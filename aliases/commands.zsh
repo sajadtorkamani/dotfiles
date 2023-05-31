5518576c921a4fd9e43a74aaac924964ff886241
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-05-31 14:55:07 +0100

    WIP

--- aliases/commands.zsh
+++ aliases/commands.zsh
@@ -11,7 +11,7 @@ alias ni="npm install"
 alias nu="npm uninstall"
 alias nid="npm install -D"
 alias nr="npm run"
-alias nclean="rm -rf node_modules package-lock.json  && npm i"
+alias nclean="rm -rf node_modules package-lock.json && npm i"
 
 # Rails
 alias r="bin/rails"
