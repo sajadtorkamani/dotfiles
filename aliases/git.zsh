c64003380df784370779dab895e18002647e63b5
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-03-03 16:26:34 +0000

    wip

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -27,6 +27,7 @@ alias gbranch="git symbolic-ref --short HEAD"
 alias gut="git ls-files -o -z --exclude-standard | xargs -0"
 alias gupstream='git branch --set-upstream-to=origin/git symbolic-ref --short HEAD`'
 alias gbranches='git for-each-ref --sort=-committerdate refs/heads/ --format="%(committerdate:iso8601) %(refname:short)" | head'
+alias gbranchesall='git for-each-ref --sort=-committerdate refs/heads/ --format="%(committerdate:iso8601) %(refname:short)"'
 
 function gbackup() {
   backupBranch="backup/$(gbranch)--$(isodate)"
