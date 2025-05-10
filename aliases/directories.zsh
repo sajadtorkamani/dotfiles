b3eed77a1d7978eb4f7e9678d3e9d7a80ec3525f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-05-10 20:21:38 +0100

    wip

--- aliases/directories.zsh
+++ aliases/directories.zsh
@@ -7,7 +7,7 @@ alias downloads="cd $HOME/Downloads && l"
 alias dumps="cd $HOME/dumps && l"
 alias fleets="cd $HOME/Downloads/Fleeting && l"
 alias home="cd ~ && l"
-alias hosts="$EDITOR /etc/hosts"
+alias hosts="sudo $EDITOR /etc/hosts"
 alias sites="cd $HOME/sites && l"
 alias vaults="cd $HOME/Google\ Drive/My\ Drive/Obsidian\ Vaults && l"
 
